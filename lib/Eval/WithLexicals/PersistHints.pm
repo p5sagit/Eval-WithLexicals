fc61569393c727b8a1abff46fc7459a08b6635dd
David Leadbeater <dgl@dgl.cx>
2011-01-11 22:20:54 +0000

    Remove this too

--- lib/Eval/WithLexicals/PersistHints.pm
+++
@@ -1,8 +0,0 @@
-# XXX: Calls for traits really; there's not a MooX::... (yet)
-package Eval::WithLexicals::PersistHints;
-use Moo;
-
-with 'Eval::WithLexicals::Role::Eval';
-with 'Eval::WithLexicals::Role::LexicalHints';
-
-1;
