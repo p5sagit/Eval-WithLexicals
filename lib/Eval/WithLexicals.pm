fdcd01808aba4c3f7200c3231a36fd78d7f444b9
Graham Knop <haarg@haarg.org>
2014-06-13 09:12:29 -0400

    bump version

--- lib/Eval/WithLexicals.pm
+++ lib/Eval/WithLexicals.pm
@@ -4,7 +4,7 @@ use Moo;
 use Moo::Role ();
 use Sub::Quote;
 
-our $VERSION = '1.002000'; # 1.2.0
+our $VERSION = '1.003000'; # 1.3.0
 $VERSION = eval $VERSION;
 
 has lexicals => (is => 'rw', default => quote_sub q{ {} });
