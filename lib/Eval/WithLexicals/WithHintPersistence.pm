bcf061be33a7a6972b6bdf0a5708714372789349
Graham Knop <haarg@haarg.org>
2014-06-13 08:38:50 -0400

    remove unused variables

--- lib/Eval/WithLexicals/WithHintPersistence.pm
+++ lib/Eval/WithLexicals/WithHintPersistence.pm
@@ -5,9 +5,6 @@ use Sub::Quote;
 our $VERSION = '1.002000'; # 1.2.0
 $VERSION = eval $VERSION;
 
-# Used localised
-our($hints, %hints);
-
 has hints => (
   is => 'rw',
   default => quote_sub q{ {} },
